495fe4221c0e7452fb1dcd66a4766be2210c3021
tharshan <tharsanrajkumar01@gmail.com>
2026-02-17 23:13:17 +0530

    learn row and column

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,19 @@ class expenseHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Expense App')),
-      body: Column(children: [Text("text1"), Text("text2"), Text("text3")]),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(children: [Text("grosaries"), Text("sep 15 2025")]),
+          Container(
+            child: Text("250.00"),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.purple, width: 2),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
